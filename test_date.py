815bf7a8dc4904bc0fe7e2735ce41f3caeb7d28a
Sharanya <63061338+SharanyaMarathe@users.noreply.github.com>
2020-04-09 15:14:27 +0530

    Update test_date.py

--- test_date.py
+++ test_date.py
@@ -1,17 +1,10 @@
 import pytest
-from date import Current_Date_info,Birth_Date_info
+from date import Date_info
+
+easy_test_cases =[("12-01-2020","2 Months from Now"),("20-05-2020","2 Months Ago"),("15-06-2020","3 Months Ago"),("25-07-2020","4 Months Ago"),("29-08-2020","5 Months Ago"),("02-09-2020","6 Months Ago"),("05-10-2020","7 Months Ago"),("30-11-2020","8 Months Ago"),("30-12-2020","9 Months Ago")]
 
 
-test_current_date=[("02-04-2020","Thursday"),("01-01-2020","Wednesday"),("11-08-2020","Tuesday"),("10-04-2020","Friday")]
-test_Birth_date=[("15-03-1995","Sunday"),("02-11-1997","Monday")]
-
-@pytest.mark.parametrize('date,exp',test_current_date)
-def test_Current_day(date,exp):
-    act=Current_Date_info(date)
-    assert act==exp
-
-
-@pytest.mark.parametrize('date,exp',test_Birth_date)
+@pytest.mark.parametrize('date,exp',easy_test_cases)
 def test_Birthday(date,exp):
-	act=Birth_Date_info(date)
+	act=Date_info(date)
 	assert act==exp
