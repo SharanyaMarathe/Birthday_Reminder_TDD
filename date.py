06182bd611236d16ca83ffb790151074546ae051
Sharanya <63061338+SharanyaMarathe@users.noreply.github.com>
2020-04-10 14:16:30 +0530

    Update date.py

--- date.py
+++ date.py
@@ -1,4 +1,4 @@
-iimport datetime
+import datetime
 
 def Birth_Date_info(B_date):
 	
@@ -12,11 +12,38 @@ def Notification(B_day,C_day):
 	if(B_day.month > C_day.month):
 		return  (str(B_day.month - C_day.month)+" Months from Now")
 
-	else: 
+	elif(B_day.month < C_day.month): 
 		return (str(C_day.month - B_day.month) +" Months Ago")
 
-	
-	#return("SomeDay")
+	else:
+		b=B_day.day
+		c=C_day.day
+		if(b-c == 7):
+			return("Next Sunday")
+
+		elif((b-c==6) or (b-c==5) or (b-c==4) or (b-c==3)):
+			return ("Coming Sunday")
+
+		elif(b-c == 2):
+			return("Day after Tomorrow")
+
+		elif(b-c == 1):
+			return("Tomorrow")
+		
+		elif((c-b==6) or (c-b==5) or (c-b==4) or (c-b==3)):
+			return(str(c-b) +" days before")
+
+		elif(c-b == 7):
+			return("Last Sunday")
+
+
+		elif(c-b == 2):
+			return("Day before Yesterday")
+
+		elif(c-b == 1):
+			return("Yesterday")
+		else:
+			return("Today")
 
 
 def Date_info(date):
@@ -34,5 +61,3 @@ def Date_info(date):
 if __name__ == "__main__":
 	dates=input("Enter the date: ")
 	Date_info(dates)
-
-
