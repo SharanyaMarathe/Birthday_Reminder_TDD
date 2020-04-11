38b8de2c85cc899878720ba799cc2882726562ec
Sharanya <63061338+SharanyaMarathe@users.noreply.github.com>
2020-04-11 12:38:46 +0530

    Add files via upload

---
+++ test_previousmonth.py
@@ -0,0 +1,10 @@
+import pytest
+from date import Date_info
+
+with open ("previousmonth_test_cases.txt",mode='r') as casefile:
+		res= [tuple(line.strip().split(","))for line in casefile]
+
+@pytest.mark.parametrize('date,exp',res)
+def test_nearby_dates(date,exp):
+	act=Date_info(date)
+	assert act==exp
