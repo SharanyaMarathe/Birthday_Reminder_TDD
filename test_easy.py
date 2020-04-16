7364f5154cf3b0e0abd0a38055efb23c4f13b2bb
Sharanya <63061338+SharanyaMarathe@users.noreply.github.com>
2020-04-16 11:47:50 +0530

    Update test_easy.py

--- test_easy.py
+++ test_easy.py
@@ -7,7 +7,7 @@ with open ("easy_test_cases.txt",mode='r') as case1file:
 
 @pytest.mark.parametrize('date,exp',res1)
 def test_easy_case(date,exp):
-	act=Date_info(date)
+	act=Date_info(date,'15-03-1955')
 	assert act==exp
 
 
