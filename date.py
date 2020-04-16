734d91294081954ac939ca2e6c94b687f78201c1
Sharanya <63061338+SharanyaMarathe@users.noreply.github.com>
2020-04-16 11:46:10 +0530

    Update date.py

--- date.py
+++ date.py
@@ -39,11 +39,11 @@ def newkeys_conditions2(conditions2):
 
 
 def Birth_Date_info(B_date):
-	
+	B_date=B_date.split("-")
 	dates_detail=datetime.date(int(2020),int(B_date.month),int(B_date.day))
 	day=dates_detail.strftime("%A")
 	month=dates_detail.strftime("%B")
-	return day,month
+	return day,dates_detail,month
 
 def Current_date_info(C_date):
 	Current_date=C_date.split("-")
@@ -81,18 +81,18 @@ def Notification(B_day,C_day):
 
 			
 
-def Date_info(date):
-	Birth_date=datetime.date(1995,3,15)
-	Birth_day=Birth_Date_info(Birth_date)
+def Date_info(date,date_birth):
+	
+	Birth_day=Birth_Date_info(date_birth)
 	Current_date = Current_date_info(date)
 	Birthday_notify=Notification(Birth_date,Current_date[0])
 	#return ("On "+day+","+ dates[0],dates_detail.strftime("%B"))
-	print("Birthday : ",Current_date[0]," ",Current_date[1]," ",Birthday_notify,"  ||  ","On ",Birth_day[0],", ",Birth_date.day,"th of",Birth_day[1])
-	
+	print("Birthday : ",Current_date[0]," ",Current_date[1]," ",Birthday_notify,"  ||  ","On ",Birth_day[0],", ",Birth_day[1].day,"th of",Birth_day[2])
 	return Birthday_notify
 
 if __name__ == "__main__":
 	dates=input("Enter the date: ")
-	Date_info(dates)
+	Birth_date=input("Enter your birthday: ")
+	Date_info(dates,Birth_date)
 
 
